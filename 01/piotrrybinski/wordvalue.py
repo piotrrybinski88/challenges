413f617f985b06ff032ff69dcfc93cb845022054
Piotr Rybiński 1 <piotr.rybinski.1@lingarogroup.com>
2019-09-17 22:20:17 +0200

    'PC001 piotrrybinski'

---
+++ 01/piotrrybinski/wordvalue.py
@@ -0,0 +1,14 @@
+from data import DICTIONARY, LETTER_SCORES
+from collections import namedtuple
+
+def find_word_value(word):
+    Word = namedtuple('Word', 'word points')
+    return Word(word, sum([LETTER_SCORES[letter] for letter in word.upper()]))
+
+
+def find_most_value_word():
+    with open(DICTIONARY, 'r') as file:
+        data = file.read()
+    return max(list(map(find_word_value, data.split())))
+
+print(find_most_value_word())
